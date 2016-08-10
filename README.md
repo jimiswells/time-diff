ea98b6f958ccd553112a15acfe74813ed06370e5
“jim <jimocmorrison@googlemail.com>
2016-08-10 16:11:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Hacking around with `/electron/electron-quick-start`.
+
+# Quick start
+
+```bash
+# Clone this repository
+git clone https://github.com/lukehefson/time-diff
+# Go into the repository
+cd time-diff
+# Install dependencies and run the app
+npm install && npm start
+```
+--
+
+![](https://cloud.githubusercontent.com/assets/1469659/16153410/17f3058a-349e-11e6-9c9a-2fc3a4ddc28f.png)
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+	   server: {
+            baseDir: "/Users/jimiswells/Desktop/WEB/time-diff"
+        }
+ 
+    });
+    
+
+    gulp.watch("/Users/jimiswells/Desktop/WEB/time-diff/assets/scss/*.scss", ['sass']);
+    gulp.watch("/Users/jimiswells/Desktop/WEB/time-diff/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("/Users/jimiswells/Desktop/WEB/time-diff/assets/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("/Users/jimiswells/Desktop/WEB/time-diff/assets/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
