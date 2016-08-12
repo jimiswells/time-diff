bee2328a1ae3d38f4d3223db1d374f822af42b5a
“jim <jimocmorrison@googlemail.com>
2016-08-12 15:29:29 +0100

    getting times into variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,22 +10,26 @@ $('.add-row').click(function () {
     });
 
 
+
+var time = moment.duration("00:03:15")
 $("#example_id").ionRangeSlider({
-    min: +moment().subtract(12, "hours").format("X"),
+     min: +moment().subtract(12, "hours").format("X"),
     max: +moment().format("X"),
     
     grid: true,
     force_edges: true,
      prettify: function (num) {
-        return moment(num, "X").format("MMM Do, hh:mm A");
+     return moment(num, "X").format("MMM Do, hh:mm A");
     }
     
 });
 
+ var now = moment().subtract(5, "hours");
 
 var nut = moment("2013-11-18").tz("America/Toronto").format('Z'); 
 console.log(nut);
 
+$('.moment').append(now._d);
 
 
 
