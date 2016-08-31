0ba67d7e8529efb959477c6691e9d56ec604a0e5
“jim <jimocmorrison@googlemail.com>
2016-08-31 22:06:38 +0100

    added searchable select dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,22 @@
 var selectIndex = 0;
 
-//ADD AND REMOVE SLIDER ROWS
-$('.add-row').click(function () {
-        var zoneSliderRow = $('.slider-row').html();
-        var deleteButton = $('.remove');
-        selectIndex = selectIndex + 1;
-        $('.content').append("<div class=\"slider-row\" id=\"\"><select id=\"city-select" + selectIndex + "\" onchange=\"change_city('" + selectIndex + "')\" data-selectNo=\"" + selectIndex + "\"><option value=\"100\">NewYork</option><option value=\"999\">china</option></select><input type=\"text\" id=\"default" + selectIndex + "\" name=\"example_name\" value=\"\" /><button class=\"remove\">X</button><div class=\"extra-controls\"><input type=\"text\" value=\"0\" class=\"inp js-from\" /><input type=\"text\"  value=\"1000\" class=\"inp js-to\" /></div></div>");
-    $('select#city-select' +selectIndex).timezones();
-    $("#default" + selectIndex).ionRangeSlider({
-    type: "single",
-    min: +moment().subtract(12, "hours").format("X"),
-    max: +moment().add(12, "hours").format("X"),
-    from: +moment.utc().format("X"),
-    prettify_enabled:true,
-    hide_min_max:true,
-    onChange: function (data) {
-        from = data.from;
-        to = data.to; 
-        updateValues();
-    },
-    prettify: function (num) {
-            return moment(num, "X").format("MMM Do, hh:mm A");}
-});
-    range = $("#default" + selectIndex).data("ionRangeSlider");
-        deleteButton.click(function () {
-            $(this).closest('div').remove();
-        });
-    });
 
 
 //GET VALUE FROM SELECT FILTER 
 //ADD ION SLIDER WITH CURRENT USERS TIME
 
 $( document ).ready(function() {
+    
+var currentZone = moment();
+    alert(currentZone);
 
 
-var $range = $("#default0"),
-    $from = $(".js-from"),
-    $to = $(".js-to"),
+var $range1 = $("#default1"),
+    $range2 = $("#default2"),
+    $from = $(".js-from1"),
+    $to = $(".js-to1"),
+    $from2 = $(".js-from2"),
+    $to2 = $(".js-to2"),
     range,
     min = +moment().subtract(12, "hours").format("X"),
     max = +moment().add(12, "hours").format("X"),
@@ -46,9 +25,32 @@ var $range = $("#default0"),
 var updateValues = function () {
     $from.prop("value", from);
     $to.prop("value", to);
+    
+    //Get timezone offset of each slider
+    var Tz1 = parseFloat($('#city-select1').find(':selected').data('offset'));
+    var Tz2 = parseFloat($('#city-select2').find(':selected').data('offset'));
+    
+    //This changes second slider
+    $from2.prop("value", from + (Tz2-Tz1)*3600  );
+    $(".js-from2").trigger("change");
+    
+};
+    
+var updateValues2 = function () {
+    $from2.prop("value", from);
+    $to2.prop("value", to);
+    
+    //Get timezone offset of each slider
+    //var Tz1 = parseFloat($('#city-select1').find(':selected').data('offset'));
+    //var Tz2 = parseFloat($('#city-select2').find(':selected').data('offset'));
+    
+    //This changes second slider
+    //$from.prop("value", from + (Tz1-Tz2)*3600  );
+    //$(".js-from1").trigger("change");
+    
 };
 
-$range.ionRangeSlider({
+$range1.ionRangeSlider({
     type: "single",
     min: min,
     max: max,
@@ -61,20 +63,79 @@ $range.ionRangeSlider({
         from = data.from;
         to = data.to; 
         updateValues();
+        //alert('hello');
+        console.log(data.from);
+        
     },
     prettify: function (num) {
             return moment(num, "X").format("MMM Do, hh:mm A");}
 });
 
-range = $range.data("ionRangeSlider");
-
+$range2.ionRangeSlider({
+    type: "single",
+    min: min,
+    max: max,
+    range:100,
+    from: +moment.utc().format("X"),
+    prettify_enabled:true,
+    grid: true,
+    grid_num: 10,
+    onChange: function (data) {
+        from = data.from;
+        to = data.to; 
+        updateValues2();
+        console.log(data.from);
+        
+    },
+    prettify: function (num) {
+            return moment(num, "X").format("MMM Do, hh:mm A");}
+});
+    
+    
+range1 = $range1.data("ionRangeSlider");
+range2 = $range2.data("ionRangeSlider");
+    
 var updateRange = function () {
-    range.update({
+    range1.update({
         from: from,
         to: to
     });
+    
+    
+    //Get timezone offset of each slider
+    var Tz1 = parseFloat($('#city-select1').find(':selected').data('offset'));
+    var Tz2 = parseFloat($('#city-select2').find(':selected').data('offset'));
+    
+    //This changes second slider
+    range2.update({
+        from: from + (Tz2-Tz1)*3600 ,
+        to: to
+    });
+    
+    
+    
+    
 };
 
+var updateRange2 = function () {
+    range2.update({
+        from: from,
+        to: to
+    });
+    
+    //Get timezone offset of each slider
+    //var Tz1 = parseFloat($('#city-select1').find(':selected').data('offset'));
+    //var Tz2 = parseFloat($('#city-select2').find(':selected').data('offset'));
+    
+    //This changes second slider
+    /*
+    range1.update({
+        from: from + (Tz1-Tz2)*3600 ,
+        to: to
+    });
+    */
+};
+    
 $from.on("change", function () {
     from = +$(this).prop("value");
     if (from < min) {
@@ -83,7 +144,6 @@ $from.on("change", function () {
     if (from > to) {
         from = to;
     }
-
     updateValues();    
     updateRange();
 });
@@ -100,49 +160,109 @@ $to.on("change", function () {
     updateValues();    
     updateRange();
 });
+    
+$from2.on("change", function () {
+    from = +$(this).prop("value");
+    if (from < min) {
+        from = min;
+    }
+    if (from > to) {
+        from = to;
+    }
+    updateValues2();    
+    updateRange2();
+});
+
+$to2.on("change", function () {
+    to = +$(this).prop("value");
+    if (to > max) {
+        to = max;
+    }
+    if (to < from) {
+        to = from;
+    }
+    updateValues2();    
+    updateRange2();
+});    
+    
+    
 
    
     });
 
- function change_city(selector_id) {
-    var timezone = $('#city-select' + selector_id).find(':selected').data('offset');
-        
+
+
+ function change_city (select_choice) {
+    var timezone = $('#city-select' + select_choice).find(':selected').data('offset');
+      //alert(timezone);
+     
       if (timezone.indexOf("+") !=-1) {
-          
             timezone = timezone.replace('+', '');
             timezone = parseFloat(timezone);
-            //alert(timezone);
             newmin = moment.utc().add(timezone, "hours").format("X");
             //alert(moment.utc().add(timezone, "hours"));
-            $(".js-from").prop("value", newmin);
-            $(".js-from").trigger("change");
+            $(".js-from" + select_choice).prop("value", newmin);
+            $(".js-from" + select_choice).trigger("change");
+          //alert(timezone);
       }
         else if(timezone.indexOf("-") !=-1) {
               
             timezone = timezone.replace('-', '');
             timezone = parseFloat(timezone);
             newmin = moment.utc().subtract(timezone, "hours").format("X");
-            $(".js-from").prop("value", newmin);
-            $(".js-from").trigger("change");
+            $(".js-from" + select_choice).prop("value", newmin);
+            $(".js-from" + select_choice).trigger("change");
             
         }
-        //alert(timezone);
-    console.log(timezone);
+     
+       
+     var title = $('select').val();
+     
+     console.log(title);
+
+     
+   
+  
+     
     }
+
 
 $('.gradient-ranger').on("change", function(){
     var gradientChange = $(this).val() + '%';
     
-    alert(gradientChange);
+
     $('.irs-with-grid').css('background', 'linear-gradient(to right,  #d4e02c 0%,#2989d8 ' + gradientChange +' ,#207cca 51%,#ebf484 100%)');
+    alert(from);
       });
+
+
 
     
 
+
+$('.add-row').click(function(){
+  
+ $('.add-row').siblings('div').closest().addClass('visible');
+    
+    
+});
+
+
  
+$('.remove2').click(function(){
+    $('.row-2').addClass('visible');
+});
+
+$('.remove3').click(function(){
+    $('.row-3').addClass('visible');
+});
 
 
 
+   
+   
+       
+    
 
 
    
--- assets/js/old.js
+++ assets/js/old.js
@@ -1,52 +1,159 @@
- var init = function(){
-        
-      
-        
-         $('#city-select').on('change', function() {
-  var timeDiff = $(this).val(); // or $(this).val()
-      
-    });
-        
+var selectIndex = 0;
 
+//ADD AND REMOVE SLIDER ROWS
+$('.add-row').click(function () {
+        var zoneSliderRow = $('.slider-row').html();
+        var deleteButton = $('.remove');
+        selectIndex = selectIndex + 1;
+        $('.content').append("<div class=\"slider-row\" id=\"\"><select id=\"city-select" + selectIndex + "\" onchange=\"change_city('" + selectIndex + "')\" data-selectNo=\"" + selectIndex + "\"><option value=\"100\">NewYork</option><option value=\"999\">china</option></select><input type=\"text\" id=\"default" + selectIndex + "\" name=\"example_name\" value=\"\" /><button class=\"remove\">X</button><div class=\"extra-controls\"><input type=\"text\" value=\"0\" class=\"inp js-from\" /><input type=\"text\"  value=\"1000\" class=\"inp js-to\" /></div></div>");
+    $('select#city-select' +selectIndex).timezones();
+    $("#default" + selectIndex).ionRangeSlider({
+    type: "single",
+    min: +moment().subtract(12, "hours").format("X"),
+    max: +moment().add(12, "hours").format("X"),
+    from: +moment.utc().format("X"),
+    prettify_enabled:true,
+    hide_min_max:true,
+    onChange: function (data) {
+        from = data.from;
+        to = data.to; 
+        $('.irs-with-grid').css('background', 'linear-gradient(to right,  #d4e02c 0%,#2989d8 '+ from +' ,#207cca 51%,#ebf484 100%)');
+        updateValues();
         
-        
-                var avalue='';
-    function getSelectData(id) {
-
-
-        // set value to be the current selected value
-        avalue = $(id+" option:selected").val();
-
-        // change value whenever the box changes
-        $(id).change(function () {
-            avalue = $(id+" option:selected").val();
-               $('.moment').append(avalue);
-              var poop = +moment().subtract(avalue, "hours").format("X")
-            
-        });
-         return avalue;
-    }
-    
-    var d = getSelectData("#city-select");
-   
-      
-          
-      var poop = +moment().subtract(avalue, "hours").format("X")
-      console.log(avalue);
-        
-     
-        $("#default").ionRangeSlider({
-          min:poop
-        , max: +moment().format("X")
-        , grid: true
-        , force_edges: true
-        , prettify: function (num) {
+    },
+    prettify: function (num) {
             return moment(num, "X").format("MMM Do, hh:mm A");}
-
-    });
-    };
-    
-    
-    
-    init();
 });
+    range = $("#default" + selectIndex).data("ionRangeSlider");
+        deleteButton.click(function () {
+            $(this).closest('div').remove();
+        });
+    });
+
+
+//GET VALUE FROM SELECT FILTER 
+//ADD ION SLIDER WITH CURRENT USERS TIME
+
+$( document ).ready(function() {
+
+
+var $range = $("#default0"),
+    $from = $(".js-from"),
+    $to = $(".js-to"),
+    range,
+    min = +moment().subtract(12, "hours").format("X"),
+    max = +moment().add(12, "hours").format("X"),
+    to;
+
+var updateValues = function () {
+    $from.prop("value", from);
+    $to.prop("value", to);
+};
+
+$range.ionRangeSlider({
+    type: "single",
+    min: min,
+    max: max,
+    range:100,
+    from: +moment.utc().format("X"),
+    prettify_enabled:true,
+    grid: true,
+    grid_num: 10,
+    onChange: function (data) {
+        from = data.from;
+        to = data.to; 
+        updateValues();
+        console.log(data.from);
+        
+    },
+    prettify: function (num) {
+            return moment(num, "X").format("MMM Do, hh:mm A");}
+});
+
+range = $range.data("ionRangeSlider");
+
+var updateRange = function () {
+    range.update({
+        from: from,
+        to: to
+    });
+};
+
+$from.on("change", function () {
+    from = +$(this).prop("value");
+    if (from < min) {
+        from = min;
+    }
+    if (from > to) {
+        from = to;
+    }
+
+    updateValues();    
+    updateRange();
+});
+
+$to.on("change", function () {
+    to = +$(this).prop("value");
+    if (to > max) {
+        to = max;
+    }
+    if (to < from) {
+        to = from;
+    }
+
+    updateValues();    
+    updateRange();
+});
+
+   
+    });
+
+ function change_city(selector_id) {
+    var timezone = $('#city-select' + selector_id).find(':selected').data('offset');
+        
+      if (timezone.indexOf("+") !=-1) {
+          
+            timezone = timezone.replace('+', '');
+            timezone = parseFloat(timezone);
+            //alert(timezone);
+            newmin = moment.utc().add(timezone, "hours").format("X");
+            //alert(moment.utc().add(timezone, "hours"));
+            $(".js-from").prop("value", newmin);
+            $(".js-from").trigger("change");
+      }
+        else if(timezone.indexOf("-") !=-1) {
+              
+            timezone = timezone.replace('-', '');
+            timezone = parseFloat(timezone);
+            newmin = moment.utc().subtract(timezone, "hours").format("X");
+            $(".js-from").prop("value", newmin);
+            $(".js-from").trigger("change");
+            
+        }
+        //alert(timezone);
+    console.log(timezone);
+    }
+
+$('.gradient-ranger').on("change", function(){
+    var gradientChange = $(this).val() + '%';
+    
+
+    $('.irs-with-grid').css('background', 'linear-gradient(to right,  #d4e02c 0%,#2989d8 ' + gradientChange +' ,#207cca 51%,#ebf484 100%)');
+    alert(from);
+      });
+
+    
+
+ 
+
+
+
+
+
+   
+
+
+
+
+    
+
